359f868bedd16d4a570f7d90a124b60021c37b8a
unknown <adamxabulkheir@gmail.com>
2023-06-21 01:45:22 -0400

    cloned and refactored code from Chronark.com repo

---
+++ app/layout.tsx
@@ -0,0 +1,70 @@
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+import { Analytics } from "./components/analytics";
+
+export const metadata: Metadata = {
+	title: {
+		default: "Adam Abulkheir - Portfolio",
+		template: "%s | Adam Abulkheir - Portfolio",
+	},
+	description: "Portfolio for beginner web-developer and graphic designer",
+	openGraph: {
+		title: "Adam Abulkheir - Portfolio",
+		description:
+			"Software engineer at upstash.com and founder of planetfall.io",
+		// url: "https://chronark.com",
+		siteName: "Adam Abulkheir - Portfolio",
+		locale: "en-US",
+		type: "website",
+	},
+	robots: {
+		index: true,
+		follow: true,
+		googleBot: {
+			index: true,
+			follow: true,
+			"max-video-preview": -1,
+			"max-image-preview": "large",
+			"max-snippet": -1,
+		},
+	},
+	twitter: {
+		title: "Adam Abulkheir - Portfolio",
+		card: "summary_large_image",
+	},
+	icons: {
+		shortcut: "/favicon.png",
+	},
+};
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+	src: "../public/fonts/CalSans-SemiBold.ttf",
+	variable: "--font-calsans",
+});
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+			<head>
+				<Analytics />
+			</head>
+			<body
+				className={`bg-black ${
+					process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+				}`}
+			>
+				{children}
+			</body>
+		</html>
+	);
+}
